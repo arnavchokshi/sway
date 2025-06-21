e84f242749533ef5b924d78870e84e943b5e85f5
arnavchokshi <chokshi.arnav@gmail.com>
2025-06-20 21:04:47 -0400

    update frontend

--- sway_frontend/src/environments/environment.prod.ts
+++ sway_frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-url.onrender.com/api', // Update this with your actual backend URL
+  apiUrl: 'https://sway-backend-3t6a.onrender.com/api',
   s3Bucket: 'sway-music-upload',
   s3Region: 'us-west-2'
 }; 
